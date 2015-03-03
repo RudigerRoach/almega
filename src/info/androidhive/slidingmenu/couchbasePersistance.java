1a9c9ecbaa97f1fb5f40cacf790ca8197ff287ff
unknown <rudiger.roach@RudigerR-PC.entelect.local>
2015-03-03 14:56:23 +0200

    couchdb can create a database, manager, insert and read

--- src/info/androidhive/slidingmenu/couchbasePersistance.java
+++ src/info/androidhive/slidingmenu/couchbasePersistance.java
@@ -2,13 +2,18 @@ package info.androidhive.slidingmenu;
 
 import android.content.Context;
 import android.util.Log;
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Document;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.android.AndroidContext;
 import info.androidhive.slidingmenu.Exceptions.globalContextNotSetException;
 import info.androidhive.slidingmenu.globalContext.contextSingleton;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by rudiger.roach on 2015-03-03.
@@ -16,15 +21,34 @@ import java.util.List;
 public class CouchbasePersistance implements PersistanceInterface {
     private  Manager manager;
     private Context context;
+    Database database;
     private final String TAG = "couchbase internal";
+    private final String DBNAME = "almegadb";
 
     public CouchbasePersistance() throws Exception {
+        setTheContext();
+        createAManager();
+        createANewDatabase();
+    }
+
+    private void setTheContext() throws globalContextNotSetException {
         context = contextSingleton.getInstance().getContext();
         if (context == null){
             throw new globalContextNotSetException();
         }
+    }
 
-        // create a manager
+    private void createANewDatabase() {
+        try {
+            database = manager.getDatabase(DBNAME);
+            Log.d(TAG, "Database created");
+        } catch (CouchbaseLiteException e) {
+            Log.e(TAG, "Cannot get database");
+            return;
+        }
+    }
+
+    private void createAManager() {
         try {
             manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS);
             Log.d(TAG, "Manager created");
@@ -33,6 +57,7 @@ public class CouchbasePersistance implements PersistanceInterface {
             return;
         }
     }
+
     @Override
     public void addClient(Client client) {
 
